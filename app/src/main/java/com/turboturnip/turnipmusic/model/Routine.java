bfe72abb32b47348ac55c7c5752ba691453937a4
theturboturnip <popgoestoast@gmail.com>
2018-10-28 13:33:47 +0000

    Started working on the model for shuffling songs

---
+++ app/src/main/java/com/turboturnip/turnipmusic/model/Routine.java
@@ -0,0 +1,11 @@
+package com.turboturnip.turnipmusic.model;
+
+import java.util.List;
+
+/**
+ * A Routine is an ordered group of Shuffles.
+ * Get it? It's a dancing metaphor (I think).
+ */
+public class Routine {
+    List<Shuffle> shuffles;
+}
---
+++ app/src/main/java/com/turboturnip/turnipmusic/model/Shuffle.java
@@ -0,0 +1,13 @@
+package com.turboturnip.turnipmusic.model;
+
+/**
+ * A Shuffle is the base unit of play in the app. Contrary to the name, it is not necessarily a shuffled set of songs,
+ * it could be a repeated set of songs, one song played once, etc.
+ */
+public interface Shuffle {
+    int getTotalLength(); // Length of -1 (or 0) implies the Shuffle can continue forever.
+    int getLengthRemaining(); // Returns the amount of songs left, if the Shuffle is bounded. Returns -1 otherwise
+
+    void advance(); // Move to the next Song.
+    Song nextSong(); // Return the Song to play next.
+}
